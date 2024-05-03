9eb0af52a36221f51cd765ed5f97fd474f0e47b7
Keerthana D <144937763+KeerthanaD11@users.noreply.github.com>
2024-05-03 18:07:11 +0530

    App.js

--- App.js
+++ App.js
@@ -7,27 +7,5 @@ function App(props) {
       <p>Student: {props.isStudent? 'Yes' : 'No'}</p>
     </div>
   );
-
-  /*return (
-    <><div className="App">
-      <h1>Welcome Home!!</h1>
-      <h1>My Website</h1>
-      <ul>
-        <li>HTML</li>
-        <li>CSS</li>
-        <li>Js</li>
-        <li>React Js</li>
-      </ul>
-    </div>
-    </>
-  );*/
-
-  /*return(
-    <div className='Card'>
-      <img className='card-image'src={image} alt='empty'></img>
-      <h1 className='card-title'>Blossom</h1>
-      <p className='card-detail'>The card of flower blossom.</p>
-    </div>
-  );*/
-}
+  
 export default App;
